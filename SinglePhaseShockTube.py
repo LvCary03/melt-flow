207973a13599b13e001c5f8103c4b5933abe2af8
Levi Cary <levicary03@gmail.com>
2025-10-13 02:57:25 -0500

    Created more functs for graph and main loop

--- SinglePhaseShockTube.py
+++ SinglePhaseShockTube.py
@@ -9,10 +9,10 @@ import math
 #Set vars
 n = 100 #   #number of grid points
 l = 10 #m   #total length of tube
-cfl = .1 #s #time step
+cfl = .01 #s #time step
 omega = (cfl + 1/cfl)/2
 
-tf = 1 #s   #time to run
+tf = .005 #s   #time to run
 
 x0 = 5 #m
 gamma = 1.4
@@ -98,7 +98,13 @@ def consToPrims(W, gamma, list_length, No_blowup):
         U[0, l] = (W[2, l] - (.5 * W[0, l] *(W[1, l]/W[0, l])**2))*(gamma - 1)
     return U, No_blowup
 
-
+def mainLoop(Wn, W, alpha, F):
+    for k in range(1, (len(alpha)-1)):
+        Wn[:, k] = (W[:, k] -
+                   (dt/(2*dx))*(F[:, k+1] - F[:, k-1]) +
+                   (1/4)*(((alpha[k+1] + alpha[k])*(W[:, k+1] - W[:, k])) - 
+                          (alpha[k] - alpha[k-1])*(W[:, k] - W[:, k-1])))
+    return Wn, W, alpha, F
 
 # sigma = max(abs(u)+c)*dt/dx
 # dt = (sigma * dx) / (max(abs(u) + c))
@@ -110,7 +116,7 @@ def consToPrims(W, gamma, list_length, No_blowup):
 # alpha = omega * (dt/dx)*(u + c)
 
 
-def calculate_alpha(list_length, cfl, dx, gamma):
+def calculate_alpha(list_length, cfl, dx, gamma): # What max do i use for alpha
     uc_max = 0
     alpha = np.zeros(list_length)
     for i in range(list_length):
@@ -136,12 +142,11 @@ def calculate_F(U, gamma, list_length):
     return F
 
 
-
 No_blowup = True
 total_time = tf
 current_time = 0
-while((current_time < total_time) and (No_blowup)):
-#for step in range(1):
+#while((current_time < total_time) and (No_blowup)):
+for step in range(1):
 
     print(U[2])
 
@@ -155,23 +160,24 @@ while((current_time < total_time) and (No_blowup)):
     Wn = W.copy()
 
 
-    for k in range(1, (len(alpha)-1)):
-        Wn[:, k] = (W[:, k] -
-                   (dt/(2*dx))*(F[:, k+1] - F[:, k-1]) +
-                   (1/4)*(((alpha[k+1] + alpha[k])*(W[:, k+1] - W[:, k])) - 
-                          (alpha[k] - alpha[k-1])*(W[:, k] - W[:, k-1])))
+    #for k in range(1, (len(alpha)-1)):
+     #   Wn[:, k] = (W[:, k] -
+      #             (dt/(2*dx))*(F[:, k+1] - F[:, k-1]) +
+       #            (1/4)*(((alpha[k+1] + alpha[k])*(W[:, k+1] - W[:, k])) - 
+        #                  (alpha[k] - alpha[k-1])*(W[:, k] - W[:, k-1])))
+    
+    Wn, W, alpha, F = mainLoop(Wn, W, alpha, F)
         
     W = Wn.copy()
 
     U, No_blowup = consToPrims(W, gamma, list_length, No_blowup)
 
+
     print(U[2])
-    #print("f1 = ", F[])
-    print("F = ", F)
+    #print("F = ", F)
   
 
     current_time += dt
-
 
 
 
@@ -182,49 +188,53 @@ if (No_blowup):
     print("The system did not blowup")
 else:
     print("The system blew up")
-#print("Density matrix =", roe)
 #print("\nConserved variables\nDensity=", W[0],'\nMomentum=', W[1], '\nEnergy=', W[2])
 #print("F = ", F)
 
 
-#Give the plots for all the variables
-fig, axs = plt.subplots(2, 2)
+def plotElementary():
+    #Give the plots for all the variables
+    fig, axs = plt.subplots(2, 2)
 
-# Plot on the first subplot (top-left)
-axs[0, 0].plot(U[1])
-axs[0, 0].set_title('Density')
+    # Plot on the first subplot (top-left)
+    axs[0, 0].plot(U[1])
+    axs[0, 0].set_title('Density')
 
-# Plot on the second subplot (top-right)
-axs[0, 1].plot(U[2], color='orange')
-axs[0, 1].set_title('Velocity')
+    # Plot on the second subplot (top-right)
+    axs[0, 1].plot(U[2], color='orange')
+    axs[0, 1].set_title('Velocity')
 
-# Plot on the third subplot (bottom-left)
-axs[1, 0].plot(U[3], color='green')
-axs[1, 0].set_title('Energy')
+    # Plot on the third subplot (bottom-left)
+    axs[1, 0].plot(U[3], color='green')
+    axs[1, 0].set_title('Energy')
 
-# Plot on the fourth subplot (bottom-right)
-axs[1, 1].plot(U[0], color='red')
-axs[1, 1].set_title('Pressure')
+    # Plot on the fourth subplot (bottom-right)
+    axs[1, 1].plot(U[0], color='red')
+    axs[1, 1].set_title('Pressure')
 
 
-fig2, axs2 = plt.subplots(2, 2)
+def plotConserved():
+    fig2, axs2 = plt.subplots(2, 2)
 
-# Plot on the first subplot (top-left)
-axs2[0, 0].plot(W[1])
-axs2[0, 0].set_title('W 1')
+    # Plot on the first subplot (top-left)
+    axs2[0, 0].plot(W[1])
+    axs2[0, 0].set_title('W 1')
 
-# Plot on the second subplot (top-right)
-axs2[0, 1].plot(W[2], color='orange')
-axs2[0, 1].set_title('W 2')
+    # Plot on the second subplot (top-right)
+    axs2[0, 1].plot(W[2], color='orange')
+    axs2[0, 1].set_title('W 2')
 
-# Plot on the third subplot (bottom-left)
-#axs2[1, 0].plot(U[3], color='green')
-#axs2[1, 0].set_title('Energy')
+    # Plot on the third subplot (bottom-left)
+    #axs2[1, 0].plot(U[3], color='green')
+    #axs2[1, 0].set_title('Energy')
 
-# Plot on the fourth subplot (bottom-right)
-axs2[1, 1].plot(W[0], color='red')
-axs2[1, 1].set_title('W 0')
+    # Plot on the fourth subplot (bottom-right)
+    axs2[1, 1].plot(W[0], color='red')
+    axs2[1, 1].set_title('W 0')
+
+
+plotElementary()
+#plotConserved()
 
 plt.show()
-
 
